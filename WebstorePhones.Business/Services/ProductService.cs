76413da4b6a274015f603ad641af01b9235f390e
Mark te Poele <mark.tepoele@itvitaelearning.nl>
2021-09-20 15:57:33 +0200

    Added sorting in list. Also assigns Id, so that it actually gives them Ids based on the newly sorted positions.

--- WebstorePhones.Business/Services/ProductService.cs
+++ WebstorePhones.Business/Services/ProductService.cs
@@ -12,7 +12,6 @@ namespace WebstorePhones.Business.Services
         {
             new Phone()
             {
-                Id = 0,
                 Brand = "Huawei",
                 Type = "P30",
                 Description = "6.47\" FHD + (2340x1080) OLED, Kirin 980 Octa - Core(2x Cortex - A76, 2.6GHz + 2x Cortex - A76 1.92GHz + 4x Cortex - A55 1.8GHz), 8GB RAM, 128GB ROM, 40 + 20 + 8 + TOF / 32MP, Dual SIM, 4200mAh, Android 9.0 + EMUI 9.1",
@@ -21,7 +20,6 @@ namespace WebstorePhones.Business.Services
             },
             new Phone()
             {
-                Id = 1,
                 Brand = "Samsung",
                 Type = "Galaxy A52",
                 Description = "64 megapixel camera, 4k videokwaliteit, 6.5 inch AMOLED scherm, 128 GB opslaggeheugen(Uitbreidbaar met Micro - sd), Water - en stofbestendig(IP67)",
@@ -30,7 +28,6 @@ namespace WebstorePhones.Business.Services
             },
             new Phone()
             {
-                Id = 2,
                 Brand = "Apple",
                 Type = "IPhone 11",
                 Description = "Met de dubbele camera schiet je in elke situatie een perfecte foto of video. De krachtige A13 - chipset zorgt voor razendsnelle prestaties. Met Face ID hoef je enkel en alleen naar je toestel te kijken om te ontgrendelen. Het toestel heeft een lange accuduur dankzij een energiezuinige processor",
@@ -39,7 +36,6 @@ namespace WebstorePhones.Business.Services
             },
             new Phone()
             {
-                Id = 3,
                 Brand = "Google",
                 Type = "Pixel 4a",
                 Description = "12.2 megapixel camera, 4k videokwaliteit, 5.81 inch OLED scherm, 128 GB opslaggeheugen, 3140 mAh accucapaciteit",
@@ -48,7 +44,6 @@ namespace WebstorePhones.Business.Services
             },
             new Phone()
             {
-                Id = 4,
                 Brand = "Xiaomi",
                 Type = "Redmi Note 10 Pro",
                 Description = "108 megapixel camera, 4k videokwaliteit, 6.67 inch AMOLED scherm, 128 GB opslaggeheugen(Uitbreidbaar met Micro - sd). Water - en stofbestendig(IP53)",
@@ -63,12 +58,27 @@ namespace WebstorePhones.Business.Services
             return Math.Round(priceAfterTax / (1 + Tax), 2);
         }
 
+        private static List<Phone> SortList(List<Phone> phones)
+        {
+            phones.Sort((x, y) => string.Compare(x.Brand, y.Brand));
+            int phoneId = 0;
+            foreach (var phone in phones)
+            {
+                phone.Id = phoneId;
+                phoneId++;
+            }
+            return phones;
+        }
+
         public static List<Phone> GetAllPhones()
         {
+
             foreach (var phone in phones)
             {
                 phone.PriceBeforeTax = CalculateBeforeTax(phone.PriceAfterTax);
             }
+            SortList(phones);
+
             return phones;
         }
 
