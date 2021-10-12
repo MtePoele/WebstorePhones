12ec60969dc564d41bb0a3b6d066d1f8150fc909
Mark te Poele <mark.tepoele@itvitaelearning.nl>
2021-10-12 15:32:33 +0200

    Database now has the data. The Get() method receives the data and the programs still work with it. Get(int id) is half changed, unsure if still needed (never has been used, asked Kenji for clarification). Search(string query) hasn't been changed at all.

--- WebstorePhones.Business/Services/PhoneService.cs
+++ WebstorePhones.Business/Services/PhoneService.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using WebstorePhones.Domain.Interfaces;
 using WebstorePhones.Domain.Objects;
@@ -7,73 +10,88 @@ namespace WebstorePhones.Business.Services
 {
     public class PhoneService : IPhoneService
     {
-        private List<Phone> phones = new()
-        {
-            new Phone()
-            {
-                Id = 1,
-                Brand = "Huawei",
-                Type = "P30",
-                Description = "6.47\" FHD + (2340x1080) OLED, Kirin 980 Octa - Core(2x Cortex - A76, 2.6GHz + 2x Cortex - A76 1.92GHz + 4x Cortex - A55 1.8GHz), 8GB RAM, 128GB ROM, 40 + 20 + 8 + TOF / 32MP, Dual SIM, 4200mAh, Android 9.0 + EMUI 9.1",
-                PriceWithTax = 697,
-                Stock = 5
-            },
-            new Phone()
-            {
-                Id = 2,
-                Brand = "Samsung",
-                Type = "Galaxy A52",
-                Description = "64 megapixel camera, 4k videokwaliteit, 6.5 inch AMOLED scherm, 128 GB opslaggeheugen(Uitbreidbaar met Micro - sd), Water - en stofbestendig(IP67)",
-                PriceWithTax = 399,
-                Stock = 13
-            },
-            new Phone()
-            {
-                Id = 3,
-                Brand = "Apple",
-                Type = "IPhone 11",
-                Description = "Met de dubbele camera schiet je in elke situatie een perfecte foto of video. De krachtige A13 - chipset zorgt voor razendsnelle prestaties. Met Face ID hoef je enkel en alleen naar je toestel te kijken om te ontgrendelen. Het toestel heeft een lange accuduur dankzij een energiezuinige processor",
-                PriceWithTax = 619,
-                Stock = 27
-            },
-            new Phone()
-            {
-                Id = 4,
-                Brand = "Google",
-                Type = "Pixel 4a",
-                Description = "12.2 megapixel camera, 4k videokwaliteit, 5.81 inch OLED scherm, 128 GB opslaggeheugen, 3140 mAh accucapaciteit",
-                PriceWithTax = 411,
-                Stock = 44
-            },
-            new Phone()
-            {
-                Id = 5,
-                Brand = "Xiaomi",
-                Type = "Redmi Note 10 Pro",
-                Description = "108 megapixel camera, 4k videokwaliteit, 6.67 inch AMOLED scherm, 128 GB opslaggeheugen(Uitbreidbaar met Micro - sd). Water - en stofbestendig(IP53)",
-                PriceWithTax = 298,
-                Stock = 1
-            }
-        };
+        const string connectionString = "Data Source=LAPTOP-I9V7KFJQ;Initial Catalog=phoneshop;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
         public Phone Get(int id)
         {
-            return phones.Single(x => id == x.Id);
+            return new Phone();
+
+            Phone phone = new Phone();
+
+            string queryString = $"SELECT * FROM phoneshop.dbo.phones WHERE ID = {id}";
+
+            using (SqlConnection connection = new(connectionString))
+            {
+                SqlCommand command = new(queryString, connection);
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        phone = ReadPhone(reader);
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+            return phone;
         }
 
         public IEnumerable<Phone> Get()
         {
+            List<Phone> phones = new();
+
+            string queryString = "SELECT * FROM phoneshop.dbo.phones";
+
+            using (SqlConnection connection = new(connectionString))
+            {
+                SqlCommand command = new(queryString, connection);
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Phone phone = ReadPhone(reader);
+                        phones.Add(phone);
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
             return phones.OrderBy(x => x.Brand);
+        }
+
+        private Phone ReadPhone(SqlDataReader reader)
+        {
+            return new()
+            {
+                Id = reader.GetInt64(0),
+                Brand = reader.GetString(1),
+                Type = reader.GetString(2),
+                Description = reader.GetString(3),
+                PriceWithTax = reader.GetDecimal(4),
+                Stock = reader.GetInt32(5)
+            };
         }
 
         public IEnumerable<Phone> Search(string query)
         {
-            query = query.ToLower();
-            return phones.Where(x =>
-                x.Brand.ToLower().Contains(query) ||
-                x.Type.ToLower().Contains(query) ||
-                x.Description.ToLower().Contains(query))
-                .OrderBy(x => x.Brand);
+            return new List<Phone>();
+
+            //query = query.ToLower();
+            //return phones.Where(x =>
+            //    x.Brand.ToLower().Contains(query) ||
+            //    x.Type.ToLower().Contains(query) ||
+            //    x.Description.ToLower().Contains(query))
+            //    .OrderBy(x => x.Brand);
         }
     }
 }
--- WebstorePhones.Domain/Objects/Phone.cs
+++ WebstorePhones.Domain/Objects/Phone.cs
@@ -4,13 +4,13 @@ namespace WebstorePhones.Domain.Objects
 {
     public class Phone
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public string Brand { get; set; }
         public string Type { get; set; }
         public string FullName { get { return $"{Brand} - {Type}"; } }
         public string Description { get; set; }
-        public double PriceWithTax { get; set; }
-        public double PriceWithoutTax { get { return Math.Round(PriceWithTax / (1 + 0.21), 2); } }
+        public decimal PriceWithTax { get; set; }
+        public decimal PriceWithoutTax { get { return Math.Round(PriceWithTax / (1 + 0.21m), 2); } }
         public int Stock { get; set; }
     }
 }
