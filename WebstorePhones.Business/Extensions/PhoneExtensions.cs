757c192da0950142549628583b704775251c5ad1
Mark te Poele <mark.tepoele@itvitaelearning.nl>
2021-11-21 17:18:18 +0100

    Started work on phone extensions.

---
+++ WebstorePhones.Business/Extensions/PhoneExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebstorePhones.Domain.Objects;
+
+namespace WebstorePhones.Business.Extensions
+{
+    public static class PhoneExtensions
+    {
+        public static decimal ttt(this Phone PriceWithoutVat, decimal PriceWithVat)
+        {
+            return Math.Round(PriceWithVat / (1 + 21), 2);
+        }
+    }
+}
