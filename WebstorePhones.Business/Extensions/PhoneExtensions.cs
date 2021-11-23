c92890598e6c9f0110cf9d8c6fc28c4854cc00bf
Mark te Poele <mark.tepoele@itvitaelearning.nl>
2021-11-23 12:04:55 +0100

    Added summary to the extension method.

--- WebstorePhones.Business/Extensions/PhoneExtensions.cs
+++ WebstorePhones.Business/Extensions/PhoneExtensions.cs
@@ -7,6 +7,12 @@ namespace WebstorePhones.Business.Extensions
     {
         private const decimal tax = 0.21m;
 
+        /// <summary>
+        /// Returns the "price without VAT" as decimal, rounded to 2 decimals.
+        /// VAT % is stored in a private const in the extension method.
+        /// </summary>
+        /// <param name="phone"></param>
+        /// <returns></returns>
         public static decimal PriceWithoutVat(this Phone phone)
         {
             return Math.Round(phone.PriceWithTax / (1 + tax), 2);
