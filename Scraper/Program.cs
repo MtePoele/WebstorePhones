75de459e03a54dbfd2681c713f33dfaf278b1680
Mark te Poele <mark.tepoele@itvitaelearning.nl>
2022-04-07 15:37:42 +0200

    Commented out all Scraper code, as it's incomplete and unused. Also removed Main's args.

--- Scraper/Program.cs
+++ Scraper/Program.cs
@@ -5,11 +5,11 @@ namespace Scraper
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            BelsimpelScraper scraper = new();
+            //BelsimpelScraper scraper = new();
 
-            var a = scraper.Execute("https://www.belsimpel.nl/API/vergelijk/v1.4/WebSearch?resultaattype=hardware_only&format=json_html_decoded");
+            //var a = scraper.Execute("https://www.belsimpel.nl/API/vergelijk/v1.4/WebSearch?resultaattype=hardware_only&format=json_html_decoded");
         }
     }
 }
