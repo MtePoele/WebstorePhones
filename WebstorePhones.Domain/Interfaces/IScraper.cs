0a4f9622bb5e25cd9635a4e3d87fd7d1a13041ec
Mark te Poele <mark.tepoele@itvitaelearning.nl>
2022-04-07 15:18:18 +0200

    Incomplete scraper implementation.

---
+++ WebstorePhones.Domain/Interfaces/IScraper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebstorePhones.Domain.Entities;
+
+namespace WebstorePhones.Domain.Interfaces
+{
+    public interface IScraper
+    {
+        public bool CanExecute(string url);
+        public Task<List<Phone>> Execute(string url);
+    }
+}
